1b3cab79741102b3cba9547d9d80cb633d03a2c5
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2018-12-03 19:35:42 +0800

    chenge log to go-log

--- basis/client/main.go
+++ basis/client/main.go
@@ -23,11 +23,11 @@ func main() {
 
 	cHandler := proto.SayServiceClient("go.micro.srv.basis", c)
 
-	resp, err := cHandler.Hello(context.TODO(), &proto.Request{Name: "wotmshuaisi"})
+	result, err := cHandler.Hello(context.Background(), &proto.Request{Name: "wotmshuaisi"})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(resp.Msg)
+	fmt.Println(result.Msg)
 }
--- basis/main.go
+++ basis/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"time"
+
+	"github.com/micro/go-log"
 
 	ocplugin "github.com/micro/go-plugins/wrapper/trace/opentracing"
 
